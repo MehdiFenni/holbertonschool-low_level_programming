e6c0e4a2559becc5248d5c62f1b8e653d873cee4
MehdiFenni <12280@holbertonstudents.com>
2025-10-26 16:35:24 +0100

    avec 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-echo '#include <stdio.h>\nint main(void){puts("\"Programming is like building a multilingual puzzle");return(0);}' > 4-puts.c && gcc 4-puts.c -o 4-puts && ./4-puts
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
