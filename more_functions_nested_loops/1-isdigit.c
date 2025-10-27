14d72a0eaa627f66af833eaf60e8711cd1b818e7
MehdiFenni <12280@holbertonstudents.com>
2025-10-27 21:22:00 +0100

    avec1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - vérifie si un caractère représente un chiffre décimal.
+ * @c: valeur entière correspondant au caractère à tester
+ *
+ * Return: 1 si c est entre '0' et '9' (inclus), sinon 0.
+ */
+int _isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
+}
