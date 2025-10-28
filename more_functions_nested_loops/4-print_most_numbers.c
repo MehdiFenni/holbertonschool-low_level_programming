7f7fcdd92375de8cb46cccf2bbdaf25e1f28aff8
MehdiFenni <12280@holbertonstudents.com>
2025-10-28 20:52:30 +0100

    4-print_most_numbers.c git commit -m 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,13 @@ void print_most_numbers(void)
 
     while (d <= '9')
     {
-	if (d != '2' && d != '4') {
+	if (d != '2' && d != '4')
 	   _putchar(d);
 
 
-    }
 	d++;
+    }
+
+
     _putchar('\n');
 }
