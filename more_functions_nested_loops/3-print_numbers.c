a2b3dd61493409118047194e1489bd5f69d9689e
MehdiFenni <12280@holbertonstudents.com>
2025-10-28 20:26:36 +0100

    avec3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 void print_numbers(void)
 {
-    char d = '0';
+	 char d = '0';
 
-    while (d <= '9')
-    {
-        _putchar(d);
-        d++;
-    }
+	while (d <= '9')
+	 {
+	_putchar(d);
+	d++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
