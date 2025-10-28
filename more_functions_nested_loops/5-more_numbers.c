1602b0f97bf830de5267b955b95c9d4d71eb04bc
MehdiFenni <12280@holbertonstudents.com>
2025-10-28 23:36:05 +0100

    avec 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,11 @@ void more_numbers(void)
 
 	for (line = 0; line < 10; line++)
 	{
-	    for (n = 0; n <= 14; n++)
+		for (n = 0; n <= 14; n++)
 	    {
-		if (n >= 10)
-		     _putchar('1');
-		_putchar((n % 10) + '0');
+			if (n >= 10)
+			    _putchar('1');
+			_putchar((n % 10) + '0');
 	    }
 	_putchar('\n');
 	}
