dace8f962351bd9e8165941537d8ea423e232bda
MehdiFenni <12280@holbertonstudents.com>
2025-10-28 20:49:25 +0100

    4-print_most_numbers.c git commit -m 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Affiche 0..9 sauf 2 et 4, puis un '\n'.
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+    char d = '0';
+
+    while (d <= '9')
+    {
+	if (d != '2' && d != '4') {
+	   _putchar(d);
+
+
+    }
+	d++;
+    _putchar('\n');
+}
