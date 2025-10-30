c834a438b1de322f0277cf6cae365cebe3d3cdbf
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 20:25:10 +0100

    modified main

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -11,5 +12,6 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void fizz_buzz(void);
 #endif
 
