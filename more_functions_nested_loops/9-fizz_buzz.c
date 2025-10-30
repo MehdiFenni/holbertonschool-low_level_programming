53895ec24df5c088f3b382627ccb3ff7e9b6c73f
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 20:08:55 +0100

    avecFizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+void fizz_buzz(void);
+
+for (int i =1; i <= 100; 1++) {
+	if (i % 15 == 0) {
+	    printf ("FizzBuzz");
+	} else if (i % 3 == 0) {
+	    printf ("Fizz");
+	} else if (i % 5 == 0) {
+	    printf ("Buzz");
+	} else {
+	    printf("%d", i);
+
+	}
+	    printf(" ");
+	}
+	printf("\n");
+}
