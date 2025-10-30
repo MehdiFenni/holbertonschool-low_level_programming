47419e9f396e97b572be0d1979fd30f98c50f214
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 21:26:34 +0100

    change FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,9 +13,58 @@ int main(void)
 	return (0);
 }
 
+#include <stdio.h>
+
+void fizz_buzz(void);
+
 /**
- * fizz_buzz - implémentation de la logique FizzBuzz.
+ * main - Point d'entrée du programme.
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+/**
+ * fizz_buzz - Affiche 1..100 en remplaçant:
+ *             multiples de 3 par "Fizz",
+ *             multiples de 5 par "Buzz",
+ *             multiples de 3 et 5 par "FizzBuzz".
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+
 void fizz_buzz(void)
 {
 	int i;
