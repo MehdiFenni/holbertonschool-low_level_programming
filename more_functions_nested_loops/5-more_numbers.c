32fa82919578acf16987e8774311034d69e581a8
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 21:56:43 +0100

    change 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,9 @@
 #include "main.h"
 
+/**
+ * more_numbers - Affiche 0..14 dix fois, chaque série suivie d'un '\n'.
+ * Return: void
+ */
 void more_numbers(void)
 {
 	int line, n;
@@ -7,11 +11,12 @@ void more_numbers(void)
 	for (line = 0; line < 10; line++)
 	{
 		for (n = 0; n <= 14; n++)
-	    {
+		{
 			if (n >= 10)
-			    _putchar('1');
+				_putchar('1');
 			_putchar((n % 10) + '0');
-	    }
-	_putchar('\n');
+		}
+		_putchar('\n');
 	}
 }
+
