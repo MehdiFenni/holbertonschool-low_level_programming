d57602f0cec3ccaa7b61f90618bacb2742427f10
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 21:49:52 +0100

    change 4-print.

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,21 +2,22 @@
 
 /**
  * print_most_numbers - Affiche 0..9 sauf 2 et 4, puis un '\n'.
+ * Description: Utilise uniquement _putchar.
  * Return: void
  */
 void print_most_numbers(void)
 {
-	char d = '0';
+    char d = '0';
 
-	while (d <= '9')
-	{
-	if (d != '2' && d != '4')
-	   _putchar(d);
+    while (d <= '9')
+    {
+        if (d != '2' && d != '4')
+        {
+            _putchar(d);
+        }
+        d++;
+    }
 
-
-	d++;
-	}
-
-
-	_putchar('\n');
+    _putchar('\n');
 }
+
