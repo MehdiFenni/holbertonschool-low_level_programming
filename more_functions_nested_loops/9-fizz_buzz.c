b0b0f3480e27ff2a2624f1a2c29a0829cdd8d9f6
MehdiFenni <12280@holbertonstudents.com>
2025-10-30 20:53:22 +0100

    change FizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,20 @@
-
 #include <stdio.h>
 
-void fizz_buzz(void);
+void fizz_buzz(void) {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 15 == 0) {
+            printf("FizzBuzz");
+        } else if (i % 3 == 0) {
+            printf("Fizz");
+        } else if (i % 5 == 0) {
+            printf("Buzz");
+        } else {
+            printf("%d", i);
+        }
 
-{
-	int i;
-
-	for (int i = 1; i <= 100; i++)
-	{
-	if (i % 15 == 0)
-	    printf ("FizzBuzz");
-	 else if (i % 3 == 0)
-	    printf ("Fizz");
-	 else if (i % 5 == 0)
-	    printf ("Buzz");
-	 else
-	    printf("%d", i);
-
-	if (i < 100)
-	    printf(" ");
-	}
-
-	printf("\n");
-
+        if (i < 100) {
+            printf(" ");
+        }
+    }
+    printf("\n");
 }
