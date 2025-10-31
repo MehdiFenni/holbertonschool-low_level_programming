88429019ea84aa6e1b2775e7e0cc613687464bd0
MehdiFenni <12280@holbertonstudents.com>
2025-10-31 09:44:54 +0100

    change 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strlen - calcule la longueur d'une chaîne C.
+ * @s: pointeur sur le premier octet de la chaîne (terminée par '\0')
+ *
+ * Return: nombre de caractères avant l’octet nul final.
+ */
 int _strlen(char *s)
 {
 	int len = 0;
