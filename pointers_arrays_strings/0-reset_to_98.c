996968efe659efd00892f268e5893ec02cb7e207
MehdiFenni <12280@holbertonstudents.com>
2025-10-31 09:19:27 +0100

    change 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+ * reset_to_98 - Met à 98 la valeur de l'entier pointé par n.
+ * @n: Pointeur vers l'entier à mettre à jour.
+ *
+ * Return: Rien.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
